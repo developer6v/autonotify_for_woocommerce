e586a0320e30f7bc37f64efa6d4a5e8ec5086557
Otávio <otaviodeveloper6v@gmail.com>
2024-10-15 17:15:47 -0300

    estilização

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -6,7 +6,10 @@ function autonotify_layout () {
     </div>
     
     <div class='autonotify-body'>
-        <span class='autonotify_title'><i class='fa-solid fa-wrench'></i> Configurações</span>
+        <div class='div_autonotify_title'>
+            <span class='autonotify_title'><i class='fa-solid fa-wrench'></i> Configurações</span>
+            <span class='autonotify_subtitle'><i class='fa-solid fa-circle-info'></i></span>    
+        </div>
         <input id = 'autonotify_token' placeholder='Informe seu token de integração Autonotify.' type = 'text'/>
         <button class='autonotify_validate_token'><i class='fa-solid fa-check-to-slot'></i> Validar Token</button>    
     </div>
