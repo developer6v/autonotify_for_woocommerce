11ec94efcc572d5b45a3b71334f67915d64be538
Otávio <otaviodeveloper6v@gmail.com>
2024-10-15 17:23:07 -0300

    styling

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -12,6 +12,7 @@ function autonotify_layout () {
         </div>
         <input id = 'autonotify_token' placeholder='Informe seu token de integração Autonotify.' type = 'text'/>
         <button class='autonotify_validate_token'><i class='fa-solid fa-check-to-slot'></i> Validar Token</button>    
+        <img class='autonotify_loading' src='../wp-content/plugins/autonotify-for-woocommerce/public/gif/loading.gif' alt='' class=''/>    
     </div>
     ";
 }
