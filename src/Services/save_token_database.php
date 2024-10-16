78d31e53b7cdb2005b3ece2abe17c9ea52f5a997
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 12:37:32 -0300

    teste

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -3,12 +3,10 @@
 
 function save_token_database ($token) {
     try {
-        return "teste";
-        /*
         $path = __FILE__ . '/../../../../../wp-admin/includes/upgrade.php';
         return "Tentando incluir: $path\n"; // Adicione isso para depuração
         //require_once $path;
-        return 'deu certo'*/
+        return 'deu certo';
 
     } catch (Exception $e) {
         return 'deu erro: ' . $e->getMessage();
