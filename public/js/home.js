347c2c93315b18daf80f95c710d432c8a25ca729
developer6v <otaviodeveloper6v@gmail.com>
2024-10-16 00:37:29 -0300

    customizaçao

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,24 @@
 jQuery(document).ready(function($){
     $('#autonotify_validate_token').click(function() {
         $('.autonotify_loading').fadeIn();
-        setTimeout(function () {
-            $('.autonotify_loading').fadeOut();
-        }, 3000)
 
+        var token = $('#autonotify_token').val();
+        $.ajax ({
+            url: "../wp-content/plugins/autonotify-for-woocommerce/src/Controllers/token.php",
+            method: "POST",
+            data: {
+                "token" : token,
+            },
+            success : function (response) {
+                console.log (response);
+            },
+            error: function (response) {
+                console.log (response);
+            }
+        }).always (function() {
+            setTimeout (function () {
+                $('.autonotify_loading').fadeOut();
+            }, 3000);
+        });
     });
 });
