e74bfc5896f7f50f78f73339cba1d949fddf7c3f
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 12:24:36 -0300

    fdsa

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -1,8 +1,10 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function save_token_database ($token) {
     try {
-        require_once '/../../../../../wp-admin/includes/upgrade.php';
+        //require_once '/../../../../../wp-admin/includes/upgrade.php';
         return 'deu certo';
 
     } catch (Exception $e) {
