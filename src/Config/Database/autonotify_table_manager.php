a22a9016b993f29a141a06e595324425c05f70b3
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 10:29:28 -0300

    teste 123

--- src/Config/Database/autonotify_table_manager.php
+++ src/Config/Database/autonotify_table_manager.php
@@ -2,7 +2,22 @@
 
 
 function autonotify_table_manager () {
-    $teste = "";
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'autonotify_config';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+        $sql = "CREATE TABLE $table_name (
+            id int(11) NOT NULL auto_increment,
+            token varchar(60) NOT NULL,
+            status varchar(60) NOT NULL,
+            PRIMARY KEY id (id) 
+        ) $charset_collate";
+
+
+		require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+    }
 }
 
 ?>
