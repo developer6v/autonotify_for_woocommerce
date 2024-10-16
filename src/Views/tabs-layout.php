0711fdd2d771d5e9b71b4d08c927ec843fc8fcf4
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 13:10:06 -0300

    teste

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -13,7 +13,7 @@ function autonotify_layout () {
     
     <div class='autonotify-body'>
         <div class='div_autonotify_title'>
-            <span class='autonotify_title'><i class='fa-solid fa-wrench'></i> Configurações</span>
+            <span class='autonotify_title'>Configurações</span>
             <span class='autonotify_subtitle' title='Configure o seu AutoNotify.'><i class='fa-solid fa-circle-info'></i></span>    
         </div>
         <span class='autonotify_token_label'><i class='fa-solid fa-key'></i> Token</span>
@@ -23,6 +23,9 @@ function autonotify_layout () {
     </div>
 
     <div class='autonotify_warning_div'>
+        <div class='autonotify_token_saved'>
+            <span><i class='fa-solid fa-check'></i> Token Validado!</span>
+        </div>
     </div>
     ";
 } 
