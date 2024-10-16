e0e27b58f6ed2c9f54d52e792fd961e54a473115
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 13:17:14 -0300

    asdflajkdsf

--- public/js/home.js
+++ public/js/home.js
@@ -10,14 +10,20 @@ jQuery(document).ready(function($){
                 "token" : token,
             },
             success : function (response) {
+                $('.autonotify_token_saved').fadeIn();
                 console.log (response);
             },
             error: function (response) {
+                $('.autonotify_token_failed').fadeIn();
                 console.log (response);
             }
         }).always (function() {
             setTimeout (function () {
                 $('.autonotify_loading').fadeOut();
+            }, 3000);
+
+            setTimeout (function () {
+                $('.autonotify_warning_div').fadeOut();
             }, 3000);
         });
     });
