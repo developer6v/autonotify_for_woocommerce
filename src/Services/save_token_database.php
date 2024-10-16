25f7665d74abfcdacb2db0704b1c30acbe9ba944
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 12:41:02 -0300

    teste

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 function save_token_database ($token) {
     try {
-        require_once '/../../../../../wp-admin/includes/upgrade.php';
+        require_once __DIR__ . '/../../../../../wp-admin/includes/upgrade.php';
         return 'deu certo';
 
     } catch (Exception $e) {
