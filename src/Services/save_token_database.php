535e2c06761ce1ac965e25533c6209764880098d
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 10:30:46 -0300

    teste 123

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -1,5 +1,5 @@
 <?php
 
 function save_token_database ($token) {
-    return "ABSPATH . '/wp-admin/includes/upgrade.php' " . $token;
+    return ABSPATH . '/wp-admin/includes/upgrade.php'  . $token;
 }
