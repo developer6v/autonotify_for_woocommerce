7c16b742dae0f802691b79a61963cc968f4187c3
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 12:16:33 -0300

    teste 123

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -1,7 +1,7 @@
 <?php
 
 function save_token_database ($token) {
-    $path = realpath(dirname(__FILE__) . '/../../../wp-admin/includes/upgrade.php');
+   require_once __FILE__ . '/../../../../../wp-admin/includes/upgrade.php';
 
-    return $path;
+    return 'deu certo';
 }
