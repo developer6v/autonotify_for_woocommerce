40dbd13fb03c22a5e88ae8044ade6882e136e128
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 13:05:13 -0300

    teste

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -16,9 +16,13 @@ function autonotify_layout () {
             <span class='autonotify_title'><i class='fa-solid fa-wrench'></i> Configurações</span>
             <span class='autonotify_subtitle' title='Configure o seu AutoNotify.'><i class='fa-solid fa-circle-info'></i></span>    
         </div>
+        <span class='autonotify_token_label'><i class='fa-solid fa-key'></i> Token</span>
         <input value = '" . $token  ."' id = 'autonotify_token' placeholder='Informe seu token de integração Autonotify.' type = 'text'/>
         <button id='autonotify_validate_token' class='autonotify_validate_token'><i class='fa-solid fa-check-to-slot'></i> Validar Token</button>    
         <img class='autonotify_loading' src='../wp-content/plugins/autonotify-for-woocommerce/public/gif/loading.gif' alt='' class=''/>    
+    </div>
+
+    <div class='autonotify_warning_div'>
     </div>
     ";
 } 
