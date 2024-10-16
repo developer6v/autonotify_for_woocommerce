7a19c54a897c2a7876209b2152770a50eeabbd44
Otávio <otaviodeveloper6v@gmail.com>
2024-10-16 12:26:47 -0300

    fdsa

--- src/Services/save_token_database.php
+++ src/Services/save_token_database.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 function save_token_database ($token) {
     try {
         $path = __FILE__ . '/../../../../../wp-admin/includes/upgrade.php';
-        echo "Tentando incluir: $path\n"; // Adicione isso para depuração
-        require_once $path;
+        return "Tentando incluir: $path\n"; // Adicione isso para depuração
+        //require_once $path;
         return 'deu certo'
 
     } catch (Exception $e) {
