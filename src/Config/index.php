8e6e52924b220a42f3e58e449e4c8af800042f16
Otávio <otaviodeveloper6v@gmail.com>
2024-11-08 14:35:03 -0300

    codign

--- src/Config/index.php
+++ src/Config/index.php
@@ -1,9 +1,9 @@
 <?php
 
 
-require_once 'Config/Database/autonotify_table_manager.php';
-require_once 'Config/Theme/assets.php';
-require_once 'Config/Theme/menu.php';
+require_once 'Database/autonotify_table_manager.php';
+require_once 'Theme/assets.php';
+require_once 'Theme/menu.php';
 require_once '../Core/hooks.php';
 require_once '../Services/index.php';
 
