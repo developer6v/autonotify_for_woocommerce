a5d0286528bf646d70f3d9997b0ab10a30528123
Otávio <otaviodeveloper6v@gmail.com>
2024-11-08 15:00:57 -0300

    estruturação

--- src/Config/Theme/assets.php
+++ src/Config/Theme/assets.php
@@ -1,7 +1,7 @@
 <?php
 function autonotify_enqueue_assets() {
-    wp_enqueue_script('homeScript', plugins_url('public/js/home.js', 'autonotify-for-woocommerce'), array(), date('YmdHis'), true);
-    wp_enqueue_style('homeStyle', plugins_url('public/css/home.css', 'autonotify-for-woocommerce'), array(), date('YmdHis'));
+    wp_enqueue_script('homeScript', plugins_url('public/js/home.js', '/autonotify-for-woocommerce'), array(), date('YmdHis'), true);
+    wp_enqueue_style('homeStyle', plugins_url('public/css/home.css', '/autonotify-for-woocommerce'), array(), date('YmdHis'));
     // Font Awesome
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css', array(), null);
 }
