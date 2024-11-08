f8241cca0e34270589d5865691479a0c586ca994
Otávio <otaviodeveloper6v@gmail.com>
2024-11-08 14:36:08 -0300

    asdf

---
+++ src/Services/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+require_once 'Api/validate_token.php';
+require_once 'Database/save_token_database.php';
+require_once 'Woocommerce/getCustomer.php';
+require_once 'Woocommerce/getOrder.php';
+
+?>
