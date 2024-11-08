02a90d7b5744f0afd96c7ffde919da85d1ab79b6
Otávio <otaviodeveloper6v@gmail.com>
2024-11-08 14:46:54 -0300

    estruturação

--- src/Config/index.php
+++ src/Config/index.php
@@ -1,9 +1,12 @@
 <?php
 
-
-require_once 'Database/autonotify_table_manager.php';
+// FrontEnd
+require_once dirname(__FILE__) . '/../Views/index.php';
 require_once 'Theme/assets.php';
 require_once 'Theme/menu.php';
+
+//Backend
+require_once 'Database/autonotify_table_manager.php';
 require_once dirname(__FILE__) . '/../Core/hooks.php';
 require_once dirname(__FILE__) . '/../Services/index.php';
 
