4d0ae6aba15d28a4bdc8fbfe44208ac51acab45d
Otávio <otaviodeveloper6v@gmail.com>
2024-11-12 14:48:27 -0300

    teste

--- src/Config/Database/autonotify_table_manager.php
+++ src/Config/Database/autonotify_table_manager.php
@@ -1,33 +1,5 @@
 <?php
 
-function autonotify_table_manager() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'autonotify_config';
-    $charset_collate = $wpdb->get_charset_collate();
-
-    if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {
-        $sql = "CREATE TABLE $table_name (
-            id int(11) NOT NULL auto_increment,
-            token varchar(60) NULL,
-            instance_key varchar(60) NULL,
-            status varchar(60) NULL,
-            PRIMARY KEY (id) 
-        ) $charset_collate";
-
-        require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
-        dbDelta($sql);
-        
-        $data = array(
-            'token' => '', 
-            'status' => '',
-            'instance_key' => ''
-        );
-
-        $wpdb->insert($table_name, $data);
-    }
-}
-
-register_activation_hook(__FILE__, 'autonotify_table_manager');
 
 
 ?>
--- src/Config/index.php
+++ src/Config/index.php
@@ -3,11 +3,41 @@
 // FrontEnd
 require_once dirname(__FILE__) . '/../Views/index.php';
 require_once 'Theme/assets.php';
-require_once 'Theme/menu.php';
+require_once 'Theme/menu.php'; 
 
 //Backend
 require_once 'Database/autonotify_table_manager.php';
 require_once dirname(__FILE__) . '/../Core/hooks.php';
 require_once dirname(__FILE__) . '/../Services/index.php';
+
+
+function autonotify_table_manager() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'autonotify_config';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    if ($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+        $sql = "CREATE TABLE $table_name (
+            id int(11) NOT NULL auto_increment,
+            token varchar(60) NULL,
+            instance_key varchar(60) NULL,
+            status varchar(60) NULL,
+            PRIMARY KEY (id) 
+        ) $charset_collate";
+
+        require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+        
+        $data = array(
+            'token' => '', 
+            'status' => '',
+            'instance_key' => ''
+        );
+
+        $wpdb->insert($table_name, $data);
+    }
+}
+
+register_activation_hook(__FILE__, 'autonotify_table_manager');
 
 ?>
