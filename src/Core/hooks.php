d5e7154be41cdddbed916e7fb20707e0b9de9bdd
Otávio <otaviodeveloper6v@gmail.com>
2024-12-05 15:55:58 -0300

    dsa

--- src/Core/hooks.php
+++ src/Core/hooks.php
@@ -4,7 +4,7 @@ add_action('woocommerce_order_status_changed', 'manage_order_status', 10, 3);
 
 function manage_order_status ($order_id, $old_status, $new_status) {
     $data = getOrderData($order_id);
-    sendAutonotify(str_replace("-", "_", $new_status), $data);
+    sendAutonotify([str_replace("-", "_", $new_status)], $data);
 }
 
 ?>
