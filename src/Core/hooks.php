70d846947bcf9607ba03c14373fa7bdee9f60802
Otávio <otaviodeveloper6v@gmail.com>
2025-01-08 13:19:08 -0300

    dfasfds

--- src/Core/hooks.php
+++ src/Core/hooks.php
@@ -17,6 +17,7 @@ function newordermanager ($order) {
 // Senha trocada
 add_action( 'woocommerce_reset_password_notification', 'custom_password_reset_email_sent', 10, 3 );
 function custom_password_reset_email_sent( $user_login, $key ) {
+    error_log('key0, ', $key);
     $user = get_user_by( 'login', $user_login );
     $data = getResetPasswordData ($user);
     sendAutonotify(['password_reset'], $data);
