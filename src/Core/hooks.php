2e3c1dcefbc53b29486ba9106d7dea52d193c60f
Otávio <otaviodeveloper6v@gmail.com>
2025-01-13 14:34:05 -0300

    hooks

--- src/Core/hooks.php
+++ src/Core/hooks.php
@@ -20,4 +20,31 @@ function custom_password_reset_email_sent( $user_login, $key ) {
     $data = getResetPasswordData ($user_login, $key);
     sendAutonotify(['password_reset'], $data);
 }
+
+
+// Carrinho Abandonado
+define('DEBUG_LOG_FILE', WP_CONTENT_DIR . '/debug-carrinho.log');
+
+// Carrinho Abandonado
+add_action('woocommerce_cart_updated', 'salvar_carrinho_na_sessao');
+function salvar_carrinho_na_sessao() {
+    if (WC()->cart && !WC()->cart->is_empty()) {
+        $cart_contents = WC()->cart->get_cart();
+        WC()->session->set('carrinho_abandonado', json_encode($cart_contents));
+        $mensagem = '[woocommerce_cart_updated] Carrinho atualizado: ' . json_encode($cart_contents);
+        file_put_contents(DEBUG_LOG_FILE, $mensagem . PHP_EOL, FILE_APPEND);
+    }
+}
+
+add_action('woocommerce_shutdown', 'verificar_carrinho_abandonado');
+function verificar_carrinho_abandonado() {
+    $carrinho = WC()->session->get('carrinho_abandonado');
+    if ($carrinho) {
+        $mensagem = '[woocommerce_shutdown] Carrinho abandonado detectado: ' . $carrinho;
+        file_put_contents(DEBUG_LOG_FILE, $mensagem . PHP_EOL, FILE_APPEND);
+    }
+}
+
+
+
 ?>
