10403e935e24163b3bf5ae9746c45ae34f0fdfc7
Otávio <otaviodeveloper6v@gmail.com>
2025-01-27 17:27:36 -0300

    table name interpolation

--- autonotify-for-woocommerce.php
+++ autonotify-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/developer6v/autonotify_for_woocommerce
  * Description:       Autonotify For Woocommerce is a plugin that allows Woocommerce to integrate with the platform Autonotify to automate WhatsApp messagesbased on orders statuses.
  * Version:           1.10.3
- * Requires at least: 5.2
+ * Requires at least: 6.2
  * Requires PHP:      7.2
  * Author:            Sourei
  * Author URI:        https://sourei.com.br/
