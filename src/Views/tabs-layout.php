68696bcb27dacc50416169cd985b49078063246e
Otávio <otaviodeveloper6v@gmail.com>
2025-01-27 17:19:56 -0300

    how tf im still up

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -10,14 +10,11 @@ function autonotify_layout() {
     $sqlStatus = $wpdb->prepare("SELECT status FROM {$table_name} WHERE id = %d", 1);
     $status = $wpdb->get_var($sqlStatus);
 
-    $escaped_token = esc_attr($token);
-    $escaped_status = esc_attr($status);
-
     echo "<div class='autonotify-header'>
         <img src='" . esc_url(plugins_url('/public/img/autonotify.svg', __FILE__)) . "' alt='' class=''/>
     </div>
 
-    <div class='autonotify-body' data-status='" . $escaped_status . "'>
+    <div class='autonotify-body' data-status='" . esc_attr($status) . "'>
 
         <div class='div_autonotify_title'>
             <span class='autonotify_title'>Configurações</span>
@@ -27,7 +24,7 @@ function autonotify_layout() {
         <div class='autonotify_body_active'>
             <span class='autonotify_token_label'><i class='fa-solid fa-key'></i> Token</span>
             <div class='autonotify_input_div'>
-                <input readonly value='" . $escaped_token . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
+                <input readonly value='" . esc_attr($token) . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
                 <i class='fa-solid fa-circle-check tokencheckedicon'></i>
             </div>
             <button class='autonotify_edit_token'><i class='fa-solid fa-pen-to-square'></i> Redefinir Token</button>        
@@ -36,7 +33,7 @@ function autonotify_layout() {
         <div class='autonotify_body_inactive'>
             <span class='autonotify_token_label'><i class='fa-solid fa-key'></i> Token</span>
             <div class='autonotify_input_div'>
-                <input value='" . $escaped_token . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
+                <input value='" . esc_attr($token) . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
             </div>    
             <button id='autonotify_validate_token' class='autonotify_validate_token'><i class='fa-solid fa-check-to-slot'></i> Validar Token</button>    
             <img class='autonotify_loading' src='" . esc_url(plugins_url('/public/gif/loading.gif', __FILE__)) . "' alt='' class=''/>
