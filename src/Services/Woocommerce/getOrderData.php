d0c04445d612369931d7a40835c02e1997ce210f
Otávio <otaviodeveloper6v@gmail.com>
2025-01-28 09:30:52 -0300

    dfasfdas

--- src/Services/Woocommerce/getOrderData.php
+++ src/Services/Woocommerce/getOrderData.php
@@ -54,7 +54,7 @@ function getOrderData($orderId) {
         "customerid" => $customer_id,                    
         "ordertotal" => number_format($order->get_total(), 2, ',', ''),
         "status" => $order->get_status(),                    
-        "createdaat" => $order->get_date_created()->gmdate('Y-m-d H:i:s'), 
+        "createdaat" => $order->get_date_created()->date('Y-m-d H:i:s'),
         "items" => $items_string, 
         "date" => gmdate("d/m/Y"),
         "hour" => gmdate("H:i:s")
