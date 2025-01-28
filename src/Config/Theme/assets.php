5448a349290e168142082813fd0c7f185bd6f33e
Otávio <otaviodeveloper6v@gmail.com>
2025-01-28 11:00:47 -0300

    font-awesome

--- src/Config/Theme/assets.php
+++ src/Config/Theme/assets.php
@@ -3,7 +3,7 @@ function autonotify_enqueue_assets() {
     wp_enqueue_script('homeScript', plugins_url('autonotify-for-woocommerce/public/js/home.js'), array(), gmdate('YmdHis'), true);
     wp_enqueue_style('homeStyle', plugins_url('autonotify-for-woocommerce/public/css/home.css'), array(), gmdate('YmdHis'));
     // Font Awesome
-    wp_enqueue_style('font-awesome', plugins_url('autonotify-for-woocommerce/public/font-awesome/css/all.min.css'), array(), gmdate('YmdHis'));
+    wp_enqueue_style('font-awesome','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), gmdate('YmdHis'));
 }
 
 add_action('admin_enqueue_scripts', 'autonotify_enqueue_assets');
