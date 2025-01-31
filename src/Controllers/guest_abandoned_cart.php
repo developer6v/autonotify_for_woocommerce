0c4ad0f66f505f444485397843a08b6d00e18bac
Otávio <otaviodeveloper6v@gmail.com>
2025-01-31 16:42:37 -0300

    asdklfadsjlk

--- src/Controllers/guest_abandoned_cart.php
+++ src/Controllers/guest_abandoned_cart.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/../../../../../wp-load.php';
 require_once __DIR__ . '/../Config/Woocommerce/abandoned-cart-hook.php';
 
 if ( class_exists( 'WooCommerce' ) ) {
-    $data = $_POST; 
+    $json_data = file_get_contents('php://input');
+    file_put_contents ('datacart1.txt', $json_data);
+    $data = json_decode($json_data, true);
     $abandoned_cart = new WC_Abandoned_Cart_Hook();
     $abandoned_cart->track_cart_on_checkout_guest($data);
     wp_send_json_success(array('message' => 'Carrinho rastreado com sucesso.'));
