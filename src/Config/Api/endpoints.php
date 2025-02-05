fa5ee3110178210029768dd8b06ca7a706984cb4
Otávio <otaviodeveloper6v@gmail.com>
2025-02-05 16:35:59 -0300

    get order data

--- src/Config/Api/endpoints.php
+++ src/Config/Api/endpoints.php
@@ -3,22 +3,23 @@
 add_action ('rest_api_init', 'autonotify_manage_endpoints');
 
 function autonotify_manage_endpoints () {
-    register_rest_route( 
+    register_rest_route(
         'autonotify/v1',
         '/token',
         array(
             'methods' => 'POST',
             'callback' => 'autonotify_endpoint_token',
+            'permission_callback' => '__return_true', 
         )
     );
 
-
-    register_rest_route( 
+    register_rest_route(
         'autonotify/v1',
         '/guest_abandoned_cart',
         array(
             'methods' => 'POST',
             'callback' => 'autonotify_endpoint_abandoned_cart',
+            'permission_callback' => '__return_true', 
         )
     );
 
