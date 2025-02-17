f40e9e030448cc4442bd51e7fd0f0d2ee309e243
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-02-17 15:46:19 -0300

    teste debug

--- src/Core/hooks.php
+++ src/Core/hooks.php
@@ -5,8 +5,8 @@
 // Mudança de Status de Pedido
 add_action('woocommerce_order_status_changed', 'autonotify_manage_order_status', 10, 3);
 function autonotify_manage_order_status ($order_id, $old_status, $new_status) {
-    file_put_contents('statuschanged.txt', $response_body . PHP_EOL, FILE_APPEND);
     $data = autonotify_getOrderData($order_id); 
+    file_put_contents('statuschanged.txt', json_encode($data, true) . PHP_EOL, FILE_APPEND);
     $status = str_replace("-", "_", $new_status);
     $status_admin = $status . "_admin";
     autonotify_sendData([$status, $status_admin], $data);
