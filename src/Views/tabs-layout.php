89979c623b00e816163098930c05dcc06782fe5f
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-02-19 10:21:35 -0300

    customizacao

--- src/Views/tabs-layout.php
+++ src/Views/tabs-layout.php
@@ -15,35 +15,46 @@ function autonotify_layout() {
         <div class='div_autonotify_title'>
             <span class='autonotify_title'>Configurações</span>
             <span class='autonotify_subtitle' title='Configure o seu AutoNotify.'>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-info-solid.svg') . "' alt='Informação' class='autonotify-icon'/>
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-info-solid.svg') . "#icon'></use>
+                </svg>
             </span>    
         </div>
 
         <div class='autonotify_body_active'>
             <span class='autonotify_token_label'>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/key-solid.svg') . "' alt='Chave' class='autonotify-icon'/> Token
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/key-solid.svg') . "#icon'></use>
+                </svg> Token
             </span>
             <div class='autonotify_input_div'>
                 <input readonly value='" . esc_attr($token) . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-check-solid.svg') . "' alt='Verificado' class='tokencheckedicon'/>
+                <svg class='tokencheckedicon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-check-solid.svg') . "#icon'></use>
+                </svg>
             </div>
             <button class='autonotify_edit_token'>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/pen-to-square-solid.svg') . "' alt='Editar' class='autonotify-icon'/> Redefinir Token
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/pen-to-square-solid.svg') . "#icon'></use>
+                </svg> Redefinir Token
             </button>        
         </div>
 
         <div class='autonotify_body_inactive'>
             <span class='autonotify_token_label'>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/key-solid.svg') . "' alt='Chave' class='autonotify-icon'/> Token
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/key-solid.svg') . "#icon'></use>
+                </svg> Token
             </span>
             <div class='autonotify_input_div'>
                 <input value='" . esc_attr($token) . "' class='autonotify_token' placeholder='Informe seu token de integração Autonotify.' type='text'/>
             </div>    
             <button id='autonotify_validate_token' class='autonotify_validate_token'>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/plug-circle-check-solid.svg') . "' alt='Validar' class='autonotify-icon'/> Validar Token
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/plug-circle-check-solid.svg') . "#icon'></use>
+                </svg> Validar Token
             </button>    
             <img class='autonotify_loading' src='" . esc_url(plugins_url('/public/gif/loading.gif', __FILE__)) . "' alt='' class=''/>
-
         </div>
 
     </div>
@@ -51,13 +62,17 @@ function autonotify_layout() {
     <div class='autonotify_warning_div'>
         <div class='autonotify_token_saved'>
             <span>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-check-solid.svg') . "' alt='Sucesso' class='autonotify-icon'/> Token Validado!
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-check-solid.svg') . "#icon'></use>
+                </svg> Token Validado!
             </span>
         </div>
         
         <div class='autonotify_token_failed'>
             <span>
-                <img src='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-exclamation.svg') . "' alt='Erro' class='autonotify-icon'/> Token Inválido!
+                <svg class='autonotify-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+                    <use xlink:href='" . esc_url(AUTONOTIFY_PLUGIN_URL . '/public/img/circle-exclamation.svg') . "#icon'></use>
+                </svg> Token Inválido!
             </span>
         </div>
     </div>";
